89badb941ef6e0fe4f436e869700613fc01984f5
StingTeamDev <97679574+StingTeamDev@users.noreply.github.com>
2022-08-20 01:25:42 +0200

    Delete Egypt-League.js

--- Egypt-League.js
+++
@@ -1,6 +0,0 @@
-  /* Egypt-League */
-  const EgyptS = await fetch(
-    "https://s1.vicious-cdn.football/ift_apis/standing/2021-2022-egyptian-premier-league.json"
-  );
-  const EgyptST = await EgyptS.json();
-  document.querySelector(".Egypt-League").innerHTML = EgyptST;
