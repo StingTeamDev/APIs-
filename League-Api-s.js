47bd9f81ef23f442067cc58690f922749705a49a
StingTeamDev <97679574+StingTeamDev@users.noreply.github.com>
2022-08-26 16:33:38 +0200

    Create League-Api-s.js

---
+++ League-Api-s.js
@@ -0,0 +1,6 @@
+document
+    .getElementsByTagName("head")[0]
+    .insertAdjacentHTML(
+      "beforeend",
+      '<link rel="stylesheet" href="https://github.com/StingTeamDev/APIs-/blob/main/League-api.css"/>'
+    );
