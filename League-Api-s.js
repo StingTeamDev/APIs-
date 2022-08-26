af3b4f461d04fe180a490cfd8589c43a9a5f52a3
StingTeamDev <97679574+StingTeamDev@users.noreply.github.com>
2022-08-26 16:36:22 +0200

    Delete League-Api-s.js

--- League-Api-s.js
+++
@@ -1,6 +0,0 @@
-document
-    .getElementsByTagName("head")[0]
-    .insertAdjacentHTML(
-      "beforeend",
-      '<link rel="stylesheet" href="https://github.com/StingTeamDev/APIs-/blob/main/League-api.css"/>'
-    );
