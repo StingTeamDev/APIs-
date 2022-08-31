855ab64ec2d7e92c467d5a90f36b99db998878cd
StingTeamDev <97679574+StingTeamDev@users.noreply.github.com>
2022-08-31 20:48:39 +0200

    Update and rename Table.js to TableTowMatch.js

---
+++ TableTowMatch.js
@@ -0,0 +1,16 @@
+const JSap = async function get() {
+  document
+    .getElementsByTagName("head")[0]
+    .insertAdjacentHTML(
+      "beforeend",
+      '<link rel="stylesheet" href="https://cdn.statically.io/gh/StingTeamDev/APIs-/main/Table.css" />'
+    );
+  const response = await fetch(
+      "https://api.anubis-dev.com/TableMatchAuto/index.php?o=5"
+    ),
+    data = await response.json();
+
+  document.querySelector(".AutoTableSting").innerHTML = data.tomorrow;
+};
+
+JSap();
